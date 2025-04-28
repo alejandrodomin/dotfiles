26edfdb109a62062adf13107ceee6347b167c236
A283le <alexdominguez169@gmail.com>
2025-04-28 13:07:04 -0400

    Highlighting is now enabled, however its missing a theme to make it pop.

--- init.lua
+++ init.lua
@@ -10,6 +10,18 @@ require("mason-lspconfig").setup()
 -- require("lspconfig").rust_analyzer.setup {}
 require("lspconfig").pyright.setup {}
 
+-- Syntax
+require'nvim-treesitter.configs'.setup {
+	auto_install = true,
+	highlight = {
+		enable = true,
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+	    	-- Using this option may slow down your editor, and you may see some duplicate highlights.
+	    	-- Instead of true it can also be a list of languages
+	    	additional_vim_regex_highlighting = false,
+  	},
+}
 
 -- Fill out terminal
 -- source: https://github.com/neovim/neovim/issues/16572
