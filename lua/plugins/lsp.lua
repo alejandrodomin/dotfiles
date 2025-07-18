a10bd56379da2e0d46248f075ac277a5679f3dc5
alejandrodomin <a283le.programmer@gmail.com>
2025-07-18 00:27:37 -0400

    I never use snippets
    Committed from iPhone

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,9 +8,6 @@ return {
 			-- main one
 			{ "ms-jpq/coq_nvim", branch = "coq" },
 
-			-- 9000+ Snippets
-			{ "ms-jpq/coq.artifacts", branch = "artifacts" },
-
 			-- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
 			-- Need to **configure separately**
 			{ "ms-jpq/coq.thirdparty", branch = "3p" },
