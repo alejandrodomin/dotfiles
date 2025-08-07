aa47f64837426b1c4dfe002e53bfd9230115414a
alejandrodomin <a283le.programmer@gmail.com>
2025-08-07 14:04:11 -0400

    don't actually like these plugins

--- nvim/lua/plugins/file.lua
+++ nvim/lua/plugins/file.lua
@@ -8,20 +8,4 @@ return {
 		branch = "harpoon2",
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
-	{
-		"lukas-reineke/headlines.nvim",
-		dependencies = "nvim-treesitter/nvim-treesitter",
-		config = true,
-		event = "VeryLazy",
-	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			"nvim-tree/nvim-web-devicons", -- optional, but recommended
-		},
-		lazy = true,
-	},
 }
